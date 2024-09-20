57ca8f11f32da2ba2cf52cba0c7201ceb12951e5
Maxim Zaglubotsky <91866807+M1sterZag@users.noreply.github.com>
2024-09-20 16:20:37 +0000

    add auth logic

--- cards/models.py
+++ cards/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -7,6 +8,7 @@ class Card(models.Model):
     is_online = models.BooleanField()
     ip = models.CharField(max_length=50)
     up_date_time = models.DateTimeField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- cards/views.py
+++ cards/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from .models import Card
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     return render(request, "cards/index.html")
 
 
+@login_required
 def servers(request):
-    servers = Card.objects.all()
+    servers = Card.objects.filter(owner=request.user)
     context = {"servers": servers}
     return render(request, "cards/servers.html", context)
