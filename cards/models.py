c6643c89db40039e2cc57b7b08df74fc28d4f824
Maxim Zaglubotsky <91866807+M1sterZag@users.noreply.github.com>
2024-09-23 09:55:56 +0000

    updated model "cards" in db

--- cards/models.py
+++ cards/models.py
@@ -4,10 +4,13 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Card(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
     is_online = models.BooleanField()
-    ip = models.CharField(max_length=50)
+    ip = models.GenericIPAddressField()
     up_date_time = models.DateTimeField()
+    port = models.PositiveIntegerField(default=80)
+    login = models.CharField(max_length=255)
+    password = models.TextField()
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
